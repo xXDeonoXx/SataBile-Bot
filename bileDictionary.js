30058c2521fe0dff9595b7b44bdff9d3fbab527d
Romullo <romullocordeiro@gmail.com>
2019-06-12 23:00:08 -0300

    bile krl

--- bileDictionary.js
+++ bileDictionary.js
@@ -2,7 +2,12 @@ const frasesDoBile = [
     'Bota natiruts aí',
     'Vai tomar no cu',
     'Não sou suas puta',
-    'O vizinho ta só a jbs'
+    'O vizinho ta só a jbs',
+    'To mandando um Zap',
+    'Deregue Johnson',
+    'Cuidado com o colono',
+    'Uma mulher sem pinto é um anjo sem asas',
+    'Tem que falar agora ou posso pensar ?'
 ];
 
 module.exports.getFraseAleatoria = function() {
