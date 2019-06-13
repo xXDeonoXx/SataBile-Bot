81e3d9740683127837de6785b73b9defeca03102
xXDeonoXx <45169227+xXDeonoXx@users.noreply.github.com>
2019-06-13 14:42:09 -0300

    Update bileDictionary.js

--- src/bileDictionary.js
+++ src/bileDictionary.js
@@ -7,7 +7,14 @@ const frasesDoBile = [
     'Deregue Johnson',
     'Cuidado com o colono',
     'Uma mulher sem pinto é um anjo sem asas',
-    'Tem que falar agora ou posso pensar ?'
+    'Tem que falar agora ou posso pensar ?',
+    'Porra cara, Aline Barros não',
+    'Bora de crescente',
+    'Um travesti sem asas é como um anjo sem pênis',
+    'Aqui o bem não prevalece, sou blindado por Satan',
+    'Farquad, Far Farquad, Far Farquad on a quad, Far quad Farquads on quad quads',
+    'É hoje que eu esbagaço uma gorila',
+    'Sou o rei da floresta'
 ];
 
 module.exports.getFraseAleatoria = function() {
