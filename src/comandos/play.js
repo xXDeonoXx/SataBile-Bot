1426a09581c840a2ace63f928a3f1aa9dcc08863
Pedro Pinho <rihor1000@gmail.com>
2019-06-13 17:28:12 -0300

    contem erros atualizado para o novo estilo

--- src/comandos/play.js
+++ src/comandos/play.js
@@ -9,29 +9,29 @@ let inVoiceChannel = false;
 const streamOptions = { seek: 0, volume: 1 };
 
 module.exports = async function (voiceChannel, link) {
-    if (!inVoiceChannel) {
-        const connection = await voiceChannel.join();
-        inVoiceChannel = true;
-        queue.push(link);
-        this.playSong(connection);
-    } else {
-        queue.push(link);
-    }
-}
+	if (!inVoiceChannel) {
+		const connection = await voiceChannel.join();
+		inVoiceChannel = true;
+		queue.push(link);
+		this.playSong(connection);
+	} else {
+		queue.push(link);
+	}
+};
 
 playSong = (connection) => {
-    // Criando stream
-    const stream = ytdl(queue[0], { filter: 'audioonly' });
-    const dispatcher = connection.playStream(stream, streamOptions);
+	// Criando stream
+	const stream = ytdl(queue[0], { filter: 'audioonly' });
+	const dispatcher = connection.playStream(stream, streamOptions);
 
-    dispatcher.on('end', () => {
-        // Removendo musica da fila
-        queue.shift();
-        // Verificando se temos mais musicas na fila
-        if (queue.length > 0) {
-            playSong(connection, queue[0]);
-        } else {
-            console.log('Não tem mais musica', queue);
-        }
-    });
-}
+	dispatcher.on('end', () => {
+		// Removendo musica da fila
+		queue.shift();
+		// Verificando se temos mais musicas na fila
+		if (queue.length > 0) {
+			playSong(connection, queue[0]);
+		} else {
+			console.log('Não tem mais musica', queue);
+		}
+	});
+};
