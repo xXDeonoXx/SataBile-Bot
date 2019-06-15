2172c806d46257014ceee2477f114c036de8d4c9
Pedro Pinho <rihor1000@gmail.com>
2019-06-15 13:57:14 -0300

    alterado index.js 🏍 atualizado os espaçamentos dos comandos de acordo com o ESLint movido a constante voiceChannel para fora do switch motivo: ESLint disse que não é uma boa pratica 🤷 removido o codigo comentado (eu sei, aquele codigo é importante, por isso movi ele para o play.js)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
 const bileDictionary = require('./bileDictionary');
 // Importando roll
 const roll = require('./comandos/roll');
@@ -18,35 +17,36 @@ const queue = new Array();
 queue.push('piruuu');
 
 client.on('message', msg => {
-
-
 	// It's good practice to ignore other bots. This also makes your bot ignore itself
 	// and not get into a spam loop (we call that "botception").
-	if(msg.author.bot) return;
-  
-	// Also good practice to ignore any message that does not start with our prefix, 
+	if (msg.author.bot) return;
+
+	// Also good practice to ignore any message that does not start with our prefix,
 	// which is set in the configuration file.
-	if(msg.content.indexOf('+') !== 0) return;
+	if (msg.content.indexOf('+') !== 0) return;
 
+	
 
-	// Here we separate our "command" name, and our "arguments" for the command. 
+	// Here we separate our "command" name, and our "arguments" for the command.
 	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
 	// command = say
 	// args = ["Is", "this", "the", "real", "life?"]
-	const args = msg.content.slice(1).trim().split(/ +/g);
+	const args = msg.content
+		.slice(1)
+		.trim()
+		.split(/ +/g);
 	const command = args.shift().toLowerCase();
 	console.log('args: ' + args);
 	console.log('\ncommand: ' + command);
-
-	switch(command){
-        
+	const voiceChannel = msg.member.voiceChannel;
+	switch (command) {
 	//retorna uma citação maravilhosa de nosso deus bile aleatóriamente
 	case 'bile':
 		msg.reply(bileDictionary.getFraseAleatoria());
 		break;
 
 		//rola dados
-	case 'roll':            
+	case 'roll':
 		msg.channel.send(msg.author + ' rolou: ' + roll.roll(args));
 		break;
 
@@ -55,32 +55,29 @@ client.on('message', msg => {
 		break;
 
 	case 'rombo':
-		msg.reply('You have been blessed by the god Rombotron. May honey drip from your holy johnson, and may succubus be attracted to you.');
+		msg.reply(
+			'You have been blessed by the god Rombotron. May honey drip from your holy johnson, and may succubus be attracted to you.'
+		);
 		break;
 
 	case 'minas':
-		msg.reply('Gabirujo just rolled in his grave, anger is bringing him back from the dead.');
+		msg.reply(
+			'Gabirujo just rolled in his grave, anger is bringing him back from the dead.'
+		);
 		break;
 
 	case 'golira':
 		msg.reply('Bile is being summoned.');
 		break;
-        
+
 	case 'play':
-		var voiceChannel = msg.member.voiceChannel;
+		if(voiceChannel === undefined) {
+			msg.reply('Ae mano na moralzinha, entra num canal de voz ai fazeno o favo.');
+			break;
+		}
 		play(voiceChannel, args[0]);
-		// const ytdl = require('ytdl-core');
-		// const streamOptions = { seek: 0, volume: 1 };
-		// voiceChannel.join().then(connection =>{
-		//     const stream = ytdl(args[0], { filter : 'audioonly' });                
-		//     const dispatcher = connection.playStream(stream, streamOptions);
-		//     dispatcher.on("end", end => {
-		//         voiceChannel.leave();
-		//     });
-		// }).catch(err => console.log(err));
 		break;
 	}
-
 });
 
 client.login(auth.token);
